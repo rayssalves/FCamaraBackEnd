0fa0fc63d9bbc9e2c92a05ac0635ae2bc9c6c1cd
Luiz Nola <luizn647@gmail.com>
2021-03-27 03:17:16 -0300

    Registrando Users no DB

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,10 @@
 import { Router } from 'express';
 
+import usersRouter from '../routes/user.routes'
+
 const routes =  Router();
 
+routes.use('/user', usersRouter)
 routes.get('/pong', (req, res) => {
     return res.json({ pong: "ping" });
 });
--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,9 @@ import routes from './routes';
 
 const app = express();
 
+import './database'
+
+app.use(express.json());
 app.use(routes);
 
 app.listen(5000, ()=> {
