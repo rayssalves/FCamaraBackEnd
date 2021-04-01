0103ce9c3c6d860d320451039f7751c80000efb1
Rayssa <rayssa.a.vieira@outlook.com>
2021-04-01 18:57:17 +0200

    Teste paginação

--- src/service/SearchStudentsService.ts
+++ src/service/SearchStudentsService.ts
@@ -7,36 +7,46 @@ class SearchStudentsService{
     public async execute(
         {searchParameters, 
         skipPagination = 0, 
-        takeMax = 2,
+        takeMax = 10,
     }:SearchStudentsInterface)
         {
         const studentsRepository = getRepository(Students);
         
-        const getAllResults = await studentsRepository.find({
-            where:[
-                { address: Like('%'+searchParameters+'%') }, 
-                { nome: Like('%'+searchParameters+'%') },
-                { material_list: Like('%'+searchParameters+'%') }
-                ]
-        })
+        // const getAllResults = await studentsRepository.find({
+        //     where:[
+        //         { address: Like('%'+searchParameters+'%') }, 
+        //         { nome: Like('%'+searchParameters+'%') },
+        //         { material_list: Like('%'+searchParameters+'%') }
+        //         ]
+        // })
 
-        const searchResults = await studentsRepository.find({
-            where:[
-                { address: Like('%'+searchParameters+'%') }, 
-                { nome: Like('%'+searchParameters+'%') },
-                { material_list: Like('%'+searchParameters+'%') }
-                ],  
-            skip:skipPagination,
-            take: takeMax
-            })
+        const searchResults = await studentsRepository.createQueryBuilder("students")
+            .where("students.address = :address", { address: `%${searchParameters}%` })
+            .andWhere("students.nome = :nome", { nome: `%${searchParameters}%` })
+            .andWhere("students.material_list = :material_list", { material_list: `%${searchParameters}%` })
+            .skip(skipPagination)
+            .take(takeMax)
+            .getMany();
+
+        return searchResults;
+
+        // const searchResults = await studentsRepository.find({
+        //     where:[
+        //         { address: Like('%'+searchParameters+'%') }, 
+        //         { nome: Like('%'+searchParameters+'%') },
+        //         { material_list: Like('%'+searchParameters+'%') }
+        //         ],  
+        //     skip:skipPagination,
+        //     take: takeMax
+        //     })
             
-            if(Math.round(getAllResults.length/takeMax) < getAllResults.length/takeMax ){
-                var pages = Math.round(getAllResults.length/takeMax) + 1
-            }else{
-                var pages = Math.round(getAllResults.length/takeMax)
-            }
+        //     if(Math.round(getAllResults.length/takeMax) < getAllResults.length/takeMax ){
+        //         var pages = Math.round(getAllResults.length/takeMax) + 1
+        //     }else{
+        //         var pages = Math.round(getAllResults.length/takeMax)
+        //     }
 
-            return {searchResults, pages}       
+        //     return {searchResults, pages}       
     }
 }
 
